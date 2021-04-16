9f3d65e008ae2cc9808ed1bbccb1aeca1c33bedd
Kyle Talbott <codekingkyle@gmail.com>
2021-04-16 08:30:00 -0400

    sas

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,3 @@
-USE burgers_db;
-
-INSERT INTO burgers (burger_name) VALUES ("Emergency Krabby Patty");
-
-INSERT INTO burgers (burger_name) VALUES ("Krabby Patty in a Can");
-
-INSERT INTO burgers (burger_name) VALUES ("All-Bun Patty");
-
-INSERT INTO burgers (burger_name) VALUES ("Krabby Double Deluxe");
-
-INSERT INTO burgers (burger_name) VALUES ("Double Triple Bossy Deluxe");
-
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meat Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', false);
