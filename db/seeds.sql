10a3568d270bfa50e34d6bce25726a3bf9f24de6
Kyle Talbott <codekingkyle@gmail.com>
2021-04-16 08:12:18 -0400

    as

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Emergency Krabby Patty");
+
+INSERT INTO burgers (burger_name) VALUES ("Krabby Patty in a Can");
+
+INSERT INTO burgers (burger_name) VALUES ("All-Bun Patty");
+
+INSERT INTO burgers (burger_name) VALUES ("Krabby Double Deluxe");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Triple Bossy Deluxe");
+
